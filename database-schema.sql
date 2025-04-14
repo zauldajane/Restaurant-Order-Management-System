f2d89ac54867b7a59e1972cce9b265b6a15dffb2
Loulong-tara <sabugowerlito@gmail.com>
2025-04-14 03:40:16 -0700

    database schema

---
+++ database-schema.sql
@@ -0,0 +1,12 @@
+create database db_batch8_capstone;
+
+use db_batch8_capstone
+
+--Table: User
+CREATE TABLE Users(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VACHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VACHAR(255) NOT NULL,
+    role ENUM('admin','customer') DEFAULT 'cutomer'
+);
