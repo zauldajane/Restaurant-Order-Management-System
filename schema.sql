1c1462cbef23dca0dbc31989946db9c893808285
Loulong-tara <sabugowerlito@gmail.com>
2025-04-21 03:55:10 -0700

    initial commit

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE db_Zaulda_project;
+
+USE db_Zaulda_project;
+
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(15),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
